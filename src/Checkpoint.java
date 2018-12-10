34aeeb4cc5169869ef2acaa4bd36f9e88b78695d
League Student <league@iMac-9.attlocal.net>
2018-12-09 16:31:22 -0800

    checkpoint done

---
+++ src/Checkpoint.java
@@ -0,0 +1,13 @@
+
+public class Checkpoint {
+	/*
+	 * 1. yes
+	 * 2. Any exception, it will catch all exceptions including ones thrown by the JRE, rather than just the ones thrown within code.
+	 * 3. The first block will always trigger, even if the exception is an ArithmeticException. The code will compile.
+	 * 4.
+	 * 	1. b
+	 * 	2. a
+	 * 	3. d
+	 * 	4. c
+	 */
+}
